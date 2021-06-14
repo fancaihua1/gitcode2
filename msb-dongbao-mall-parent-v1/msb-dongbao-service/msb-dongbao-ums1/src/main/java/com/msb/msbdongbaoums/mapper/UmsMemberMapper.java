4c3c895798c234a2c65be503c8d4ff7512e75009
fch <953960708@qq.com>
2021-06-14 10:23:02 +0800

    提交项目代码

---
+++ msb-dongbao-mall-parent-v1/msb-dongbao-service/msb-dongbao-ums1/src/main/java/com/msb/msbdongbaoums/mapper/UmsMemberMapper.java
@@ -0,0 +1,16 @@
+package com.msb.msbdongbaoums.mapper;
+
+import com.msb.msbdongbaoums.entity.UmsMember;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+ * <p>
+ * 后台用户表 Mapper 接口
+ * </p>
+ *
+ * @author 晁鹏飞
+ * @since 2021-06-12
+ */
+public interface UmsMemberMapper extends BaseMapper<UmsMember> {
+
+}
---
+++ msb-dongbao-mall-parent-v1/msb-dongbao-service/msb-dongbao-ums1/src/main/java/com/msb/msbdongbaoums/service/UmsMemberService.java
@@ -0,0 +1,16 @@
+package com.msb.msbdongbaoums.service;
+
+import com.msb.msbdongbaoums.entity.UmsMember;
+import com.baomidou.mybatisplus.extension.service.IService;
+
+/**
+ * <p>
+ * 后台用户表 服务类
+ * </p>
+ *
+ * @author 晁鹏飞
+ * @since 2021-06-12
+ */
+public interface UmsMemberService extends IService<UmsMember> {
+
+}
